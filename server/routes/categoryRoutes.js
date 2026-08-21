00aa40b86e48aa1edb7d569a344a43493b366991
Buildora Industry <95603737+LainaIndustry@users.noreply.github.com>
2026-08-21 12:09:21 +0530

    Delete server directory

--- server/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controllers/categoryController');
-
-router.get('/', categoryController.getAllCategories);
-router.get('/:slug', categoryController.getCategoryBySlug);
-
-module.exports = router;
--- server/routes/unitRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const unitController = require('../controllers/unitController');
-
-router.get('/', unitController.getAllUnits);
-router.get('/category/:categoryId', unitController.getUnitsByCategory);
-
-module.exports = router;
